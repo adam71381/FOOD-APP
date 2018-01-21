14a6fc710be03956cab87b1305ad7ee0e50c450c
adamzhang <adamzcyyyyyyy@gmail.com>
2018-01-21 11:24:22 -0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,73 @@
+// var myNumber = 3;
+// var fruit = "pear"
+// console.log("There are"+(myNumber*3)+" "+fruit);
+
+/*var	a=1;
+var b = 2;
+if (a>b) {
+	console.log('A is greater than B');
+}	else	{console.log('B is greater than A')}
+
+var score = prompt('What\'s your score?');
+
+if (score>=50){
+	console.log('You passes!');
+}	else {
+	console.log('Keep trying...');
+}*/
+
+/*prompt*/
+// var food = "apple"
+// var yourfood = prompt("你最爱次的食物是?");
+// if (food===yourfood) {
+// 	alert("good taste!");
+// }	else {
+// 	console.log("YOCK");
+// }
+
+// var age = prompt("你在干什么？");
+// if (age == "复习") {
+// 	console.log("好学生");
+// }	else if (age == "吃饭"){
+// 	console.log ("饿了?");
+// }	else {
+// 	console.log("boring!")
+// }
+
+
+// for (var number = 7;
+// 	number > 0;
+// 	number--){
+// 	console.log(number)
+// }
+
+
+// for (var i = 15; i <= 30; i++) {
+// 	console.log("Studnet number" + i)
+// }
+
+// function subtract(a,b){
+// 	var c = a-b;
+// 	return c;
+// }
+
+// var x = function (a,b) {return a+b};
+// var z = x (8,4);
+// alert(z)
+
+// $("h1").text("Where to?");
+// jQuery(doucement).ready(function(){
+// 	<code内容>加上这个显示文件
+// })
+
+// $("h1").attr("id","title")
+// $("h1").css("color":"blue";"backround":"red")
+// $("h1").css("blue","green")
+
+// $(".btn").click(function(){
+// 	$("h1").text("new text");
+// 	});
+$(".btn").click(function(){
+	$("h1").fadeOut("_");
+	
+	});
